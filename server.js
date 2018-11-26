c9999c67f80eadeb64e7a4dc2c06441b75c1fce4
tarun.murala <tarun.murala@servicenow.com>
2018-11-25 20:32:30 -0800

    Including Test Server for AWS

--- server.js
+++ server.js
@@ -141,6 +141,6 @@ app.get('/contact', function (req, res) {
 // console.log('Server running on http://%s:%s', ip, port);
 
 // AWS Config
-app.listen(8080, () => console.log('Server running on port 8080'));
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 module.exports = app ;
